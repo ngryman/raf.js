34a99046c295bc351ded723a0887d072130839ef
Nicolas Gryman <ngryman@gmail.com>
2013-08-08 15:50:36 -0400

    fixed #1, #2, use of cancelAnimationFrame & removed useless Date.now cast.

--- raf.js
+++ raf.js
@@ -13,21 +13,20 @@
 	var lastTime = 0,
 		vendors = ['webkit', 'moz'],
 		requestAnimationFrame = window.requestAnimationFrame,
-		cancelAnimationFrame = window.cancelRequestAnimationFrame,
+		cancelAnimationFrame = window.cancelAnimationFrame,
 		i = vendors.length;
 
 	// try to un-prefix existing raf
 	while (--i >= 0 && !requestAnimationFrame) {
 		requestAnimationFrame = window[vendors[i] + 'RequestAnimationFrame'];
-		cancelAnimationFrame = window[vendors[i] + 'CancelRequestAnimationFrame'];
+		cancelAnimationFrame = window[vendors[i] + 'CancelAnimationFrame'];
 	}
 
 	// polyfill with setTimeout fallback
 	// heavily inspired from @darius gist mod: https://gist.github.com/paulirish/1579671#comment-837945
 	if (!requestAnimationFrame || !cancelAnimationFrame) {
 		requestAnimationFrame = function(callback) {
-			var now = +Date.now(),
-				nextTime = Math.max(lastTime + 16, now);
+			var now = Date.now(), nextTime = Math.max(lastTime + 16, now);
 			return setTimeout(function() {
 				callback(lastTime = nextTime);
 			}, nextTime - now);
@@ -38,5 +37,5 @@
 
 	// export to window
 	window.requestAnimationFrame = requestAnimationFrame;
-	window.cancelRequestAnimationFrame = cancelAnimationFrame;
+	window.cancelAnimationFrame = cancelAnimationFrame;
 }(window));
